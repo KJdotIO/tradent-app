aa8464d487bba309864cbec087b5e194ed66c1be
KJdotIO <98490529+KJdotIO@users.noreply.github.com>
2023-11-09 02:45:36 +0000

    minor changes

--- pages/SignIn.tsx
+++ pages/SignIn.tsx
@@ -32,7 +32,7 @@ const SignIn = () => {
         <div className="flex justify-center">
           <div className="alert alert-success absolute top-5 max-w-[40%] h-[160px] flex justify-center">
             <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-            <span>You've been Signed In</span>
+            <span>You&apos;ve been Signed In</span>
           </div>
         </div>
       }
